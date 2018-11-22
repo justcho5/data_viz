6400bc4684ae7d2b46a0f0bf1751d2641d98d008
Kirusanth Poopalasingam <code@kiru.io>
2018-11-22 20:40:43 +0100

    added code

--- assets/js/filter_by_category.js
+++ assets/js/filter_by_category.js
@@ -1,7 +1,7 @@
 function filter_by_category(id) {
-	
-	const circles = d3.selectAll("circle").filter("." + id);
 
-	circles.transition().style("visibility", 
-		(document.getElementById(id).checked) ? "visible" : "hidden");
+    const circles = d3.selectAll("circle").filter("." + id);
+
+    circles.transition().style("visibility",
+        (document.getElementById(id).checked) ? "visible" : "hidden");
 }
--- server/app.py
+++ server/app.py
@@ -3,18 +3,72 @@
 © Kirusanth Poopalasingan ( info@kiru.io )
 '''
 import flask as f
+import pandas as pd
+import sqlalchemy as sql
 import pickle
+from flask_cors import CORS
 
+engine = sql.create_engine('sqlite:///database.db')
+metadata = sql.MetaData()
+articles = sql.Table('articles', metadata,
+    sql.Column('id', sql.String),
+    sql.Column('title', sql.String),
+    sql.Column('year', sql.Integer),
+    sql.Column('month', sql.Integer),
+    sql.Column('day', sql.Integer),
+    sql.Column('view_count', sql.Integer),
+    sql.Column('main_category', sql.String)
+)
+metadata.create_all(engine)
 app = f.Flask(__name__)
 
+CORS(app)
 
 @app.route('/')
-def parse():
-    with open('data/counts_apr.pkl', 'rb') as f:
-        data = pickle.load(f)
-        print(data)
+def index():
+    return "Hello World."
 
-    return "Hello World"
+# @app.route('/all')
+# def all_view_count():
+#     with open('data/counts_apr.pkl', 'rb') as f:
+#         data = pickle.load(f)
+#         for key in data:
+#             wiki_id = key
+#             hours = data[wiki_id]
+#             print(hours[0])
+#             print(key, len(hours))
+#             break
+#         #print(data)
+#
+#     return "Hello World"
+
+@app.route('/topArticles/<start_year>/<start_month>/<end_year>/<end_month>')
+def all_view_count(start_year, start_month,
+                   end_year, end_month):
+
+    result = []
+    with engine.connect() as con:
+        rows = con.execute('''
+            select id, sum(view_count), title
+            from articles
+              where (year * 100 + month) >= (100 * :sy + :sm)
+                and (year * 100 + month) <= (100 * :ey + :em)
+            group by id
+            order by sum(view_count) desc
+            limit 50
+        ''', dict(sy=int(start_year), sm=int(start_month),
+              ey=int(end_year), em=int(end_month)))
+
+        for r in rows:
+            bla = {"article_id": r[0],
+                   "article_name": r[2],
+                   "view_count": r[1],
+                   "main_category": ""
+                   }
+            result.append(bla)
+            print(bla)
+
+    return f.jsonify(result)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
