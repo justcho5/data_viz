9288a2f594cd5f756c8688053fe35855fb2512d9
Justina Cho <justcho5@gmail.com>
2018-12-21 16:12:22 +0100

    new db

--- server/app.py
+++ server/app.py
@@ -10,7 +10,7 @@ from flask_cors import CORS
 import wikipedia
 import json
 
-engine = sql.create_engine('sqlite:///database_working.db')
+engine = sql.create_engine('sqlite:///database_working_add_052017.db')
 metadata = sql.MetaData()
 articles = sql.Table('articles', metadata,
     sql.Column('title', sql.String),
